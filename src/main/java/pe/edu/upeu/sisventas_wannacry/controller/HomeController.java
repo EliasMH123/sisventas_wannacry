927afd1d63915e3ee219fb05e3fb3a25dd4fb3e9
elias <elias.mamani@upeu.edu.pe>
2020-09-17 10:53:25 -0500

    v1

--- src/main/java/pe/edu/upeu/sisventas_wannacry/controller/HomeController.java
+++ src/main/java/pe/edu/upeu/sisventas_wannacry/controller/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String mensaje() {
-        return "ohhhh me vengo";
+        return "gaaaaaaaaaaaaaaaaaaa";
     }
 }
