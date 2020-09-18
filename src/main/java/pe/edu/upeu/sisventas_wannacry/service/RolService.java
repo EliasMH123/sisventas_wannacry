fcee8db30c29b62493af19aeda4b95c608ae306f
elias <elias.mamani@upeu.edu.pe>
2020-09-18 10:46:58 -0500

    Semana02Sesion2

--- src/main/java/pe/edu/upeu/sisventas_wannacry/service/RolService.java
+++ src/main/java/pe/edu/upeu/sisventas_wannacry/service/RolService.java
@@ -1,5 +1,15 @@
 package pe.edu.upeu.sisventas_wannacry.service;
 
+import java.util.List;
+import java.util.Map;
+
+import pe.edu.upeu.sisventas_wannacry.entity.Rol;
+
 public interface RolService {
 
+	int create(Rol r);
+	int update(Rol r);
+	int delete(int id);
+	Rol read(int id);
+	List<Map<String,Object>> readAll();
 }
--- src/test/java/pe/edu/upeu/sisventas_wannacry/SisventasWannacryApplicationTests.java
+++ src/test/java/pe/edu/upeu/sisventas_wannacry/SisventasWannacryApplicationTests.java
@@ -1,13 +1,25 @@
 package pe.edu.upeu.sisventas_wannacry;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import pe.edu.upeu.sisventas_wannacry.dao.RolDao;
+import pe.edu.upeu.sisventas_wannacry.entity.Rol;
+import pe.edu.upeu.sisventas_wannacry.service.RolService;
 
 @SpringBootTest
 class SisventasWannacryApplicationTests {
-
+	@Autowired
+	private RolService rolDao;
 	@Test
 	void contextLoads() {
+		/*
+		Rol r = new Rol();
+		r.setNomrol("Administrador");
+		System.out.println(rolDao.create(r));
+		*/
+		System.out.println(rolDao.readAll());
 	}
 
 }
